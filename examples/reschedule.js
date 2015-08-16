0c99977ddd0e63c2b5f76cbb965f50b0fae72ab1
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2015-08-17 02:14:52 +0300

    lint

--- examples/reschedule.js
+++ examples/reschedule.js
@@ -17,16 +17,16 @@ var eventTriggered = function (err, key) {
 // Create key 'reschedule' in Redis to expire in 3000 ms and run 'eventTriggered' handler.
 scheduler.schedule({ key: 'reschedule', expire: 3000, handler: eventTriggered }, function (err) {
   if (err) {
-    console.error(err)
+    console.error(err);
   } else {
     console.log('the task is scheduled to be run in 3000 ms');
   }
 });
 
 // Change expire time to 5000 ms
-scheduler.reschedule({ key: 'reschedule', expire: 5000 }, function () {
+scheduler.reschedule({ key: 'reschedule', expire: 5000 }, function (err) {
   if (err) {
-    console.error(err)
+    console.error(err);
   } else {
     console.log('the task is rescheduled to be run in 5000 ms');
   }
